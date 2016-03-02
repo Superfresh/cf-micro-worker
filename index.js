ae51577aee86b1455fd226c154e4fb697c870d7c
vzaidman <bmokDy>
2016-03-02 12:15:58 +0200

    updated the job runner project

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const config = require('./config');
-const jobInterval = config.jobInterval;
-const mongoUrl = config.mongoUrl;
+const JOB_INTERVAL = process.env.JOB_INTERVAL || (1000 * 6);
+const MONGO_URL = process.env.MONGO_URL || 'localhost/cf-reactjs-jumpstart';
 
 const escapeRegexp = require('escape-regexp');
 const monk = require('monk');
 const debug = require('debug')('job:main');
 
 
-const db = monk(mongoUrl);
+const db = monk(MONGO_URL);
 const todos = db.get('todos');
+const suspicious = db.get('suspicious');
 
 const prohibitedWords = require('./prohibitedWords.json');
 const prohibitedWordsRegex = new RegExp(
-  prohibitedWords.map(t => `\\b${escapeRegexp(t)}\\b`).join('|'),
-  'ig'
+        prohibitedWords.map(t => `\\b${escapeRegexp(t)}\\b`).join('|'),
+    'ig'
 );
 
 
 main();
 
 function main(){
-  const timerId = setInterval(job, jobInterval);
+  const timerId = setInterval(job, JOB_INTERVAL);
   job();
 }
 
@@ -33,9 +33,19 @@ function job(){
   todos.find({}, function(err, todoLists){
     todoLists.forEach(function(todoList){
       todoList.todos.forEach(function(todo){
-        console.log(todo.text);
+
         if(prohibitedWordsRegex.test(todo.text)){
-          console.warn(`Prohibited words detected '${todo.text}' in list '${todoList._id}', todo id '${todo.id}'`);
+          console.warn(`Prohibited words detected '${todo.text}' in list '${todoList.name}', todo id '${todo.id}'`);
+          suspicious.findAndModify(
+              { todoId : todo.id},
+              { listId: todoList._id,
+                listName: todoList.name,
+                todoId: todo.id,
+                todoText: todo.text
+              },
+              { upsert: true }
+          );
+
         }
       });
     });
